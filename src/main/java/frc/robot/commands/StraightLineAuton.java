d53f70685b37be3e2fc95609f00250a9430ff76d
Zixi Qiao <qiaozixi@gmail.com>
2024-12-23 16:36:18 -0500

    fixed path

--- src/main/java/frc/robot/commands/StraightLineAuton.java
+++ src/main/java/frc/robot/commands/StraightLineAuton.java
@@ -11,7 +11,7 @@ public class StraightLineAuton extends SequentialCommandGroup {
 
     public StraightLineAuton(Drivetrain drivetrain) {
         this.drivetrain = drivetrain;
-        trajectory = TrajectoryLoader.getTrajectory("./output/BasicAuton.wpilib.json");
+        trajectory = TrajectoryLoader.getTrajectory("./deploy/output/BasicAuton.wpilib.json");
         addCommands(new DrivetrainRamsete(drivetrain, trajectory));
     }
 }
